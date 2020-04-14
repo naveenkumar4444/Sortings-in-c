04518ebd8d58284567fb5fa76734c6d79ced1b72
naveenkumar4444 <naveenkumar00004444@gmail.com>
2020-04-14 21:02:11 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
 
+void insertion_sort();
+
+int n;
+
+int main()
+{
+    int i;
+    
+    printf("\nEnter size of an array: ");
+    scanf("%d", &n);
+    
+    int a[n];
+    
+    printf("\nEnter elements of an array: ");
+    
+    for(i=0; i<n; i++)
+        scanf("%d", &a[i]);
+        
+    printf("\nBefore sorting:\n");
+    
+    for(i=0; i<n; i++)
+        printf("%d ", a[i]);
+        
+    insertion_sort(a);
+    
+    printf("\nAfter sorting:\n");
+    
+    for(i=0; i<n; i++)
+        printf("%d ", a[i]);
+}
+void insertion_sort(int a[n])
+{
+    int i, j, temp;
+    for(i=1; i<n; i++)
+    {
+        temp = a[i];
+        j=i;
+        while (j>0 && a[j-1]>temp)
+        {
+            a[j] = a[j-1];
+            j--;
+        }
+        a[j] = temp;
+    }
+}
